9319cdfc7084d36a506e0f57242e0563cb4c920c
Socrates Palma <dubstepero10@gmail.com>
2022-03-26 19:06:24 -0600

    adding styles to footer

--- src/components/layout/Footer/Footer.styled.js
+++ src/components/layout/Footer/Footer.styled.js
@@ -3,12 +3,60 @@ import tw from 'twin.macro';
 
 export const StyledFooter = styled.div`
   ${tw`
-    bg-[#fe5e41ff]
+    bg-[#f4f4f4]
+    px-16
+    py-8
   `}
 `;
 
 export const StyledSocialIcons = styled.section`
   ${tw`
     flex
+    text-3xl
+    gap-x-3
   `}
+`;
+
+export const StyledFooterNavs = styled.nav`
+  ${tw`
+  flex
+  flex-col
+  w-52
+  gap-2
+`}
+
+  h2 {
+    font-weight: 500;
+  }
+
+  .navlink:hover {
+    color: #4a4e69;
+  }
+`;
+
+export const StyledFooterGrid = styled.section`
+  ${tw`
+  grid
+  gap-y-8
+  py-8
+`}
+  grid-template-columns: repeat(auto-fit, minmax(13rem, 1fr));
+`;
+
+export const StyledFooterLogo = styled.div`
+  ${tw`
+  text-2xl
+  font-semibold
+  text-[#0f1525]
+`}
+  font-family: 'HelloVintage', sans-serif;
+`;
+
+export const StyledCopyright = styled.div`
+  ${tw`
+  text-center
+  pb-8
+  text-[#4a4e69]
+  text-sm
+`}
 `;
--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -3,39 +3,83 @@ import getYear from '@utils/getYear';
 import React from 'react';
 import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { StyledFooter, StyledSocialIcons } from './Footer.styled';
+import {
+  StyledCopyright,
+  StyledFooter,
+  StyledFooterGrid,
+  StyledFooterLogo,
+  StyledFooterNavs,
+  StyledSocialIcons
+} from './Footer.styled';
 
 export default function Footer() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
   return (
     <StyledFooter>
-      <h1>Amapola Escarlata</h1>
-      <h2>How can we help?</h2>
-      <Link to="/pages/help">FAQs</Link>
-      <Link to="/pages/contact-us">Contact us</Link>
-      <Link to="/policies/return-policy">Return policy</Link>
-      <Link to="/policies/shipping-policy">Shipping policy</Link>
-      <h2>My account</h2>
-      <Link to="/account/login">Login</Link>
-      <Link to="/account/register">Sign up</Link>
-      <h2>Our company</h2>
-      <Link to="/pages/about-us">About us</Link>
-      <Link to="/policies/terms-of-service">Terms of service</Link>
-      <Link to="/policies/privacy-policy">Privacy policy</Link>
-      <h2>Be the first to know</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <button type="submit">Sign up</button>
-      </form>
-      <h2>Connect with us</h2>
-      <StyledSocialIcons>
-        <FaFacebook />
-        <FaInstagram />
-        <FaWhatsapp />
-      </StyledSocialIcons>
-      <p>&copy;{getYear()} Amapola Escarlata. All Rights Reserved</p>
+      <StyledFooterLogo>
+        <h1>Amapola Escarlata</h1>
+      </StyledFooterLogo>
+      <StyledFooterGrid>
+        <StyledFooterNavs>
+          <h2>How can we help?</h2>
+
+          <Link className="navlink" to="/pages/help">
+            FAQs
+          </Link>
+          <Link className="navlink" to="/pages/contact-us">
+            Contact us
+          </Link>
+          <Link className="navlink" to="/policies/return-policy">
+            Return policy
+          </Link>
+          <Link className="navlink" to="/policies/shipping-policy">
+            Shipping policy
+          </Link>
+        </StyledFooterNavs>
+        <StyledFooterNavs>
+          <h2>My account</h2>
+          <Link className="navlink" to="/account/login">
+            Login
+          </Link>
+          <Link className="navlink" to="/account/register">
+            Sign up
+          </Link>
+        </StyledFooterNavs>
+        <StyledFooterNavs>
+          <h2>Our company</h2>
+          <Link className="navlink" to="/pages/about-us">
+            About us
+          </Link>
+          <Link className="navlink" to="/policies/terms-of-service">
+            Terms of service
+          </Link>
+          <Link className="navlink" to="/policies/privacy-policy">
+            Privacy policy
+          </Link>
+        </StyledFooterNavs>
+        <StyledFooterNavs>
+          <h2>Connect with us</h2>
+          <StyledSocialIcons>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/Amapola-Escarlata-1765011270283166"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/amapola_escarlata/"
+            >
+              <FaInstagram />
+            </a>
+            <FaWhatsapp />
+          </StyledSocialIcons>
+        </StyledFooterNavs>
+      </StyledFooterGrid>
+      <StyledCopyright>
+        <p>&copy;{getYear()} Amapola Escarlata. All Rights Reserved</p>
+      </StyledCopyright>
     </StyledFooter>
   );
 }
