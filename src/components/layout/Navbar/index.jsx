affec78659cb609c6851cdfe8d2768d510d5207e
Socrates Palma <dubstepero10@gmail.com>
2022-03-29 23:52:10 -0600

    adding some of navbar logic

--- src/components/layout/Navbar/index.jsx
+++ src/components/layout/Navbar/index.jsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import { FiSearch, FiShoppingBag } from 'react-icons/fi';
+import React, { useEffect, useRef, useState } from 'react';
+import { FiMenu, FiSearch, FiShoppingBag, FiX } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
 import { StyledNavbar } from './Navbar.styled';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [size, setSize] = useState(window.innerWidth);
+  const navRef = useRef(null);
+  const showNavRef = useRef(null);
+
+  const checkSize = () => setSize(window.innerWidth);
+
+  const handleClick = () =>
+    size < 1024 ? setIsOpen((prevState) => !prevState) : setIsOpen(true);
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+
+    if (size < 1024) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+
+    return () => {
+      window.removeEventListener('resize', checkSize);
+    };
+  }, [size]);
+
+  useEffect(() => {
+    if ((isOpen && size < 1024) || size >= 1024) {
+      // navRef
+      // showNavRef
+    } else {
+      // navRef
+      // showNavRef
+    }
+  }, [isOpen]);
+
   return (
     <>
-      <StyledNavbar>
-        <ul>
-          <NavLink className="navlink" to="/">
+      <StyledNavbar ref={navRef}>
+        <ul ref={showNavRef}>
+          <NavLink className="navlink" to="/" onClick={handleClick}>
             Dresses
           </NavLink>
-          <NavLink className="navlink" to="/">
+          <NavLink className="navlink" to="/" onClick={handleClick}>
             Clothing
           </NavLink>
-          <NavLink className="navlink" to="/">
+          <NavLink className="navlink" to="/" onClick={handleClick}>
             Accesories
           </NavLink>
-          <NavLink className="navlink" to="/">
+          <NavLink className="navlink" to="/" onClick={handleClick}>
             Collections
           </NavLink>
-          <NavLink className="navlink" to="/account/login">
+          <NavLink
+            className="navlink"
+            to="/account/login"
+            onClick={handleClick}
+          >
             Log In
           </NavLink>
         </ul>
@@ -33,9 +71,12 @@ export default function Navbar() {
         <FiShoppingBag />
       </button>
 
-      {/* <button type="button">
-        <FiMenu />
-      </button> */}
+      <button
+        type="button"
+        onClick={() => setIsOpen((prevState) => !prevState)}
+      >
+        {isOpen ? <FiX /> : <FiMenu />}
+      </button>
     </>
   );
 }
